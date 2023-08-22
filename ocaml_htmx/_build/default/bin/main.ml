eb1ae573f57ecaf182d4b617efc05cc7e0be743f
chechyotka <romanchechyotkin@gmail.com>
2023-08-22 23:45:27 +0300

    added updating and deleting

--- ocaml_htmx/_build/default/bin/main.ml
+++ ocaml_htmx/_build/default/bin/main.ml
@@ -5,17 +5,46 @@ let elt_to_string elt = Fmt.str "%a" (Tyxml.Html.pp_elt()) elt;;
 type todo = {
   id: int;
   title: string;
-  is_done: bool; 
+  mutable is_done: bool; 
 }
 
 let storage: todo list ref = ref [];;
 
+let is_completed is_done = 
+  match is_done with
+  | true -> "line-through"
+  | false -> ""
+;;
+
 let display_row row = 
   let open Tyxml.Html in 
-  let ocaml = li ~a:[a_class ["flex"; "gap-1"] ] [
-    div [ txt (Int.to_string row.id) ]; 
-    div [ txt (row.title) ]; 
-    div [ txt (Bool.to_string row.is_done) ]; 
+  let ocaml = li ~a:[ 
+    a_class ["flex"; "gap-1"; "w-fit"];
+    a_id (Printf.sprintf "todo%d" row.id);
+    Unsafe.string_attrib "hx-target" "this"; 
+    Unsafe.string_attrib "hx-swap" "outerHTML"; 
+  ]
+  [
+    div ~a:[ 
+      a_class ["flex"; "gap-1"; "w-fit"; is_completed row.is_done]; 
+      Unsafe.string_attrib "hx-post" (Printf.sprintf "/update/%d" row.id);
+      Unsafe.string_attrib "hx-trigger" "click"; 
+    ] 
+    [
+      div [ txt (Int.to_string row.id) ]; 
+      div [ txt (row.title) ]; 
+      div [ txt (Bool.to_string row.is_done) ]; 
+    ];
+    button ~a:[ 
+      a_class ["border-black"];
+      Unsafe.string_attrib "hx-target" (Printf.sprintf "#todo%d" row.id); 
+      Unsafe.string_attrib "hx-swap" "delete"; 
+      Unsafe.string_attrib "hx-post" (Printf.sprintf "/delete/%d" row.id);
+      Unsafe.string_attrib "hx-trigger" "click"; 
+    ] 
+    [ 
+      txt "delete" 
+    ]
   ] in 
   ocaml
 ;;
@@ -28,15 +57,14 @@ let display_storage storage =
 
 let create_form () = 
   let open Tyxml.Html in
-  let ocaml = 
-    input() ~a:[ 
-      a_placeholder "name"; 
-      a_input_type `Text; 
-      a_name "input";
-      Unsafe.string_attrib "hx-post" "/create";
-      Unsafe.string_attrib "hx-trigger" "click[ctrlKey]"; 
-      Unsafe.string_attrib "hx-target" "#list"; 
-      Unsafe.string_attrib "hx-swap" "beforeend"; 
+  let ocaml = input() ~a:[ 
+    a_placeholder "name"; 
+    a_input_type `Text; 
+    a_name "input";
+    Unsafe.string_attrib "hx-post" "/create";
+    Unsafe.string_attrib "hx-trigger" "click[ctrlKey]"; 
+    Unsafe.string_attrib "hx-target" "#list"; 
+    Unsafe.string_attrib "hx-swap" "beforeend"; 
   ] in
   ocaml
 ;;
@@ -49,6 +77,20 @@ let create_todo (name: string) =
   } in
   storage := t :: !storage; 
   display_row t
+;;
+
+let change_is_done t = 
+  t.is_done <- true
+;;
+
+let complete_todo id =
+  let t = List.find (fun t -> t.id = id) !storage in
+  change_is_done t;
+  display_row t
+;;
+
+let delete_todo id =
+  storage := List.filter (fun t -> t.id != id) !storage
 ;;
 
 let index storage = 
@@ -77,7 +119,6 @@ let main () =
 
     Dream.get "/" (fun _ -> 
       (
-        List.iter (fun t -> Dream.log "%d %s %b" t.id t.title t.is_done) !storage;
         Dream.html @@ elt_to_string (index (List.rev !storage))
       ));
 
@@ -87,6 +128,17 @@ let main () =
       | `Ok ["input", value] ->
         Dream.html @@ elt_to_string (create_todo value)
       | _ -> Dream.empty `Bad_Request
+    );
+
+    Dream.post "/update/:id" (fun request -> 
+      let id = Dream.param request "id" in
+      Dream.html @@ elt_to_string (complete_todo (int_of_string id))
+    );
+
+    Dream.post "/delete/:id" (fun request -> 
+      let id = Dream.param request "id" in
+      delete_todo (int_of_string id);
+      Dream.empty `OK
     )
 
   ]
